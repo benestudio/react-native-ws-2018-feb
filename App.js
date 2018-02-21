f8f04a0ff5e64aee150ac688a40d6548dbc00175
Adam Bene <adambene@adambene.com>
2018-02-21 23:49:33 +0100

    integrated listing into app with mock data

--- App.js
+++ App.js
@@ -2,18 +2,45 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Search from './src/Components/Search';
+import Listing from './src/Components/Listing';
+import searchMock from './src/api/searchMock';
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      songs: [],
+    };
+  }
+
+  async componentDidMount() {
+    const newSongs = await searchMock({
+      offset: 0,
+      limit: 100,
+      q: 'Shpongle',
+    });
+
+    this.setState({
+      songs: newSongs,
+    });
+  }
+
   handleSearchChange(text) {
     console.log('search text is', text);
   }
 
   render() {
+    const { songs } = this.state;
+
     return (
       <View style={styles.container}>
         <Text>Welcome at Bene Studio! Enjoy coding.</Text>
         <Search
           onChange={text => this.handleSearchChange(text)}
+        />
+        <Listing
+          items={songs}
         />
       </View>
     );
