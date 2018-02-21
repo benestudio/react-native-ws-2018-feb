63d6831bdd1219feb8b394937a030d0b60d15e12
Adam Bene <adambene@adambene.com>
2018-02-22 00:01:43 +0100

    added separator to the list

--- src/Components/Listing.js
+++ src/Components/Listing.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import { FlatList, View, Text } from 'react-native';
 
 import ListItem from './ListItem';
+import Separator from './Separator';
 
 export default ({ items }) => (
   <FlatList
     data={items}
     renderItem={({ item }) => <ListItem item={item} />}
     keyExtractor={item => item.id}
+    ItemSeparatorComponent={() => <Separator />}
   />
 );
---
+++ src/Components/Separator.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { FlatList, View, Text } from 'react-native';
+
+export default ({ items }) => (
+  <View
+    style={{
+      height: 1,
+      backgroundColor: 'green',
+      marginTop: 10,
+      marginBottom: 10,
+    }}
+  />
+);
