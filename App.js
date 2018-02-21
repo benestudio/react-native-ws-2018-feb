5ed5031785fea1ac9ba32fa564000d0c91be0ded
Adam Bene <adambene@adambene.com>
2018-02-21 23:39:28 +0100

    align app screen with flexbox

--- App.js
+++ App.js
@@ -24,7 +24,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'stretch',
+    justifyContent: 'flex-start',
+    margin: 10,
+    marginTop: 50,
   },
 });
